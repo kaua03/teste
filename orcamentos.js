1daec774eb875f237ec2c8c21729ad9fb615fc31
kaua03 <125662694+kaua03@users.noreply.github.com>
2026-09-02 13:31:23 -0300

    Update orcamentos.js

--- orcamentos.js
+++ orcamentos.js
@@ -1182,7 +1182,7 @@ function abrirModalCadastro(tipo) {
     
     if (tipo === 'cliente') {
         titulo.innerHTML = '<i class="ph-bold ph-user-plus mr-2"></i>Cadastrar Novo Cliente';
-        btnSalvar.innerHTML = '<i class="ph-bold ph-check"></i> Salvar Cliente';
+       // btnSalvar.innerHTML = '<i class="ph-bold ph-check"></i> Salvar Cliente';
         conteudo.innerHTML = `
         <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
             <div class="md:col-span-2">
@@ -1226,7 +1226,7 @@ function abrirModalCadastro(tipo) {
         </div>`;
     } else {
         titulo.innerHTML = '<i class="ph-bold ph-jeep mr-2"></i>Cadastrar Novo Veículo';
-        btnSalvar.innerHTML = '<i class="ph-bold ph-check"></i> Salvar Veículo';
+        //btnSalvar.innerHTML = '<i class="ph-bold ph-check"></i> Salvar Veículo';
         
         let optionsDono = '<option value="">Sem vínculo / Selecione o Proprietário...</option>';
         const clienteOS = document.getElementById('db-cliente-nome').value;
@@ -1312,7 +1312,7 @@ async function buscarCEP(cepInput) {
 
 async function processarSalvamentoModal() {
     const btnSalvar = document.querySelector('#modal-cadastro-rapido button:last-child');
-    const textoOriginal = modalTipoAberto === 'cliente' ? '<i class="ph-bold ph-check"></i> Salvar Cliente' : '<i class="ph-bold ph-check"></i> Salvar Veículo';
+    //const textoOriginal = modalTipoAberto === 'cliente' ? '<i class="ph-bold ph-check"></i> Salvar Cliente' : '<i class="ph-bold ph-check"></i> Salvar Veículo';
     btnSalvar.innerHTML = '<i class="ph-bold ph-spinner animate-spin"></i> Salvando...';
     btnSalvar.disabled = true;
 
